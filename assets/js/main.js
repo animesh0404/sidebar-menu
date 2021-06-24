c7c576b51d8c025b88d590065def11d8a474b523
Animesh Kahara <animeshcyberphysco@gmail.com>
2021-06-24 07:59:28 +0530

    Active Nav link color persists on selection

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,12 @@ const showMenu = (toggleId, navbarId, bodyId) => {
   }
 };
 showMenu("nav-toggle", "navbar", "body");
+
+// Link Active color
+const linkColor = document.querySelectorAll(".nav__link");
+
+function colorLink() {
+  linkColor.forEach(L => L.classList.remove("active"));
+  this.classList.add("active");
+}
+linkColor.forEach(L => L.addEventListener("click", colorLink));
